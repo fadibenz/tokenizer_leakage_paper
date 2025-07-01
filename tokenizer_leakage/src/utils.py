589160c48e83d63dbc7e248a16650156a895c306
fadi benzaima <f.benzaima@esi-sba.dz>
2025-07-01 17:20:27 +0300

    feat(utils/lr_scheduler): Add Cosine annealing learning rate scheduler with tests
    Introduces a learning rate scheduler based on the LambdaLR functionality in torch.optim.lr_scheduler,
    the implemented scheduler uses cosine annealing with warmup for efficient training.
    
    A dedicated test suite has been integrated, it is adapted from the public CS336 course repository

--- tokenizer_leakage/src/utils.py
+++ tokenizer_leakage/src/utils.py
@@ -2,6 +2,9 @@ import torch
 import numpy as np
 import random
 import yaml
+import math
+import torch.optim as optim
+
 
 def set_seed_everything(seed: int = 2025):
     """Sets the seed for reproducibility."""
@@ -17,3 +20,19 @@ def load_config(path: str) -> dict:
     """Loads a YAML configuration file."""
     with open(path, 'r') as f:
         return yaml.safe_load(f)
+
+def get_lr_scheduler(optimizer, warmup_steps, annealing_steps, max_lr, min_lr):
+    """Cosine annealing with warmup learning rate scheduler."""
+
+    def lr_lambda(current_step):
+        ratio = min_lr / max_lr
+        if current_step < warmup_steps:
+            return float(current_step) / float(max(1, warmup_steps))
+        elif current_step < annealing_steps:
+            progress = float(current_step - warmup_steps) / float(annealing_steps - warmup_steps)
+            return ratio + 0.5 * (1.0 + math.cos(math.pi * progress)) * (1 - ratio)
+        else:
+            return ratio
+
+    return optim.lr_scheduler.LambdaLR(optimizer, lr_lambda)
+
