c77df4513db4513996332162442ff81e7b71cbf3
fadi benzaima <f.benzaima@esi-sba.dz>
2025-07-01 14:53:37 +0300

    feat(create_model): Add function to create a Llama-style model based on a config file.

---
+++ tokenizer_leakage/src/model.py
@@ -0,0 +1,18 @@
+from transformers import LlamaForCausalLM, LlamaConfig
+
+def create_model(run_config: dict) -> LlamaForCausalLM:
+    """Creates a Llama-style model based on a configuration file."""
+    if not run_config:
+        raise ValueError(f"No config file was provided, you need to specify config file to create model")
+
+    config =  LlamaConfig(
+        hidden_size=run_config["hidden_size"],
+        intermediate_size=run_config["intermediate_size"],
+        num_hidden_layers=run_config["num_hidden_layers"],
+        num_attention_heads=run_config["num_attention_heads"],
+        vocab_size=run_config["vocab_size"],
+        max_position_embeddings= run_config["context_length"],
+        rms_norm_eps= 1e-6
+    )
+
+    return LlamaForCausalLM(config)
