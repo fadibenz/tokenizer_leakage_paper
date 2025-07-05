694260ca5d9f37395debfaa7609ae1db8dd16d8a
fadi benzaima <f.benzaima@esi-sba.dz>
2025-07-05 15:02:09 +0300

    fix(train): Fix pbar display

--- tokenizer_leakage/src/evaluation.py
+++ tokenizer_leakage/src/evaluation.py
@@ -1,3 +1,5 @@
+import time
+
 import torch
 import numpy as np
 from tqdm import tqdm
@@ -18,24 +20,33 @@ def evaluate_perplexity(
     total_val_loss = 0
     num_val_batches = 0
 
-    with tqdm(data_loader,
-              desc="Evaluating Perplexity",
-              disable=not xm.is_master_ordinal(),
-              leave=False,
-              ncols=80) as pbar:
-        for x, y in pbar:
-            with torch.autocast(device_type=device.type, dtype=torch.bfloat16):
-                logits = model(x).logits
-                loss = F.cross_entropy(logits.view(-1, logits.size(-1)), y.view(-1))
-                del logits
-                total_val_loss += loss.item()
-                del loss
-                num_val_batches += 1
+    pbar = tqdm(
+        total=len(data_loader),
+        desc="Evaluating Perplexity",
+        disable=not xm.is_master_ordinal(),
+        ncols=120
+    )
+    for x, y in data_loader:
+        start = time.time()
+        with torch.autocast(device_type=device.type, dtype=torch.bfloat16):
+            logits = model(x).logits
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), y.view(-1))
+            del logits
+            total_val_loss += loss.item()
+            del loss
 
-            xm.mark_step()
+        num_val_batches += 1
+        xm.mark_step()
+        duration = time.time() - start
 
-            if num_val_batches % 10 == 0:
-                gc.collect()
+        if xm.is_master_ordinal():
+            pbar.update(1)
+            pbar.set_postfix({"duration": duration})
+
+
+        if num_val_batches % 10 == 0:
+            gc.collect()
+
 
     total_loss_tensor = torch.tensor([total_val_loss], dtype=torch.float32, device=device)
     num_batches_tensor = torch.tensor([num_val_batches], dtype=torch.float32).to(device)
@@ -46,4 +57,5 @@ def evaluate_perplexity(
     avg_loss = total_loss_tensor.item() / num_batches_tensor.item() if num_batches_tensor.item() > 0 else 0.0
     perplexity = np.exp(avg_loss)
 
+    pbar.close()
     return avg_loss, perplexity
